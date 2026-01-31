5b1258ca50ecf44d4cced85923948d645db3c693
vcat <1991416497@qq.com>
2026-01-31 20:27:53 +0800

    格式化代码

--- core/server.go
+++ core/server.go
@@ -1,25 +1,25 @@
 package core
 
 import (
-    "Server/conf"
-    "fmt"
+	"Server/conf"
+	"fmt"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 // RunHTTP 启动 HTTP 服务
 func RunHTTP(cfg *conf.Config) error {
-    r := gin.Default()
+	r := gin.Default()
 
-    r.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "pong",
-            "port":    cfg.Server.Port,
-        })
-    })
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+			"port":    cfg.Server.Port,
+		})
+	})
 
-    addr := fmt.Sprintf(":%d", cfg.Server.Port)
-    fmt.Printf("Starting server on %s\n", addr)
+	addr := fmt.Sprintf(":%d", cfg.Server.Port)
+	fmt.Printf("Starting server on %s\n", addr)
 
-    return r.Run(addr)
+	return r.Run(addr)
 }
